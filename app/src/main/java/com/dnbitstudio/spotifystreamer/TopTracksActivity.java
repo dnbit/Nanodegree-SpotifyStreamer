7e4918f094ab417e61541aaa54b3259e815ee526
dnbit <byakugan359@gmail.com>
2015-08-15 17:49:06 +0100

    fix: remove unnecessary menu items

--- app/src/main/java/com/dnbitstudio/spotifystreamer/TopTracksActivity.java
+++ app/src/main/java/com/dnbitstudio/spotifystreamer/TopTracksActivity.java
@@ -3,8 +3,6 @@ package com.dnbitstudio.spotifystreamer;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import com.dnbitstudio.spotifystreamer.models.CustomTrack;
 
@@ -37,31 +35,6 @@ public class TopTracksActivity extends AppCompatActivity implements TopTracksAct
             getSupportFragmentManager().beginTransaction()
                     .replace(R.id.top_tracks_container, topTracksFragment).commit();
         }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu)
-    {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_top_tracks, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings)
-        {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @Override
